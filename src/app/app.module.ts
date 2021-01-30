0336a55c3ce43df31269a86ba9a949a72d2b2f3e
Rakia Ben Sassi <rakia.bensassi@gmail.com>
2021-01-30 21:37:50 +0100

    update code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule   } from '@angular/platform-browser';
+import { NgModule        } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
+import { AngularMaterialModule       } from './shared/angular-material.module';
 import { AppRoutingModule            } from './app-routing.module';
 import { AppComponent                } from './app.component';
 import { CreateStep1Component        } from './orders/create-step1/create-step1.component';
 import { CreateStep2Component        } from './orders/create-step2/create-step2.component';
-import { CreateStepperComponent } from './orders/create-stepper/create-stepper.component';
-import {DynamicStepperModule} from '../../projects/dynamic-stepper/src/lib/dynamic-stepper.module';
+import { CreateStepperComponent      } from './orders/create-stepper/create-stepper.component';
+import { DynamicStepperModule        } from '../../projects/dynamic-stepper/src/lib/dynamic-stepper.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import {DynamicStepperModule} from '../../projects/dynamic-stepper/src/lib/dynam
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DynamicStepperModule
+    DynamicStepperModule,
+    AngularMaterialModule,
+    TranslateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
