bb3d9917ac002e7339b11f075c8dc7dab58cef9b
Rakia Ben Sassi <rakia.bensassi@gmail.com>
2021-02-05 16:36:22 +0100

    cleanup

--- projects/dynamic-stepper/src/public-api.ts
+++ projects/dynamic-stepper/src/public-api.ts
@@ -4,5 +4,4 @@
 
 export * from './lib/components/dynamic-stepper/dynamic-stepper.component';
 export * from './lib/components/steps-header/steps-header.component';
-export * from './lib/dynamic-stepper.service';
 export * from './lib/dynamic-stepper.module';
