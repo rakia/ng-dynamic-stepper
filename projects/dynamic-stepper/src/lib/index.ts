838c02696b8f7e2cb7d28ed72e9fcbdc5f7bb77f
rakia <rakia.bensassi@gmail.com>
2021-09-20 21:27:14 +0200

    Make hand-gesture.service visible to the outside calling modules (export hand-gesture.service)

--- projects/dynamic-stepper/src/lib/index.ts
+++ projects/dynamic-stepper/src/lib/index.ts
@@ -1,2 +1,3 @@
 export * from './components/dynamic-stepper/dynamic-stepper.component';
 export * from './models/step.model';
+export * from './ml/hand-gesture.service';
--- projects/dynamic-stepper/src/public-api.ts
+++ projects/dynamic-stepper/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/components/dynamic-stepper/dynamic-stepper.component';
 export * from './lib/components/steps-header/steps-header.component';
 export * from './lib/dynamic-stepper.module';
+export * from './lib/ml/hand-gesture.service';
